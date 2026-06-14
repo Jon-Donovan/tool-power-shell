eeabe993c2066a6d091bbf7c1912729ce02e5829
Jon-Donovan <coderline@gmail.com>
2026-06-15 00:56:31 +0300

    feat(repo): add repo setup command
    * add PowerShell command to run configured setup script
    * add example config for setup script path
    * ignore local config and diff files

---
+++ Public/Invoke-RepoSetup.ps1
@@ -0,0 +1,27 @@
+﻿function Invoke-RepoSetup {
+    [CmdletBinding()]
+    param(
+        [string]$ConfigPath = ".\config.json"
+    )
+
+    if (-not (Test-Path $ConfigPath)) {
+        throw "Конфиг не найден: $ConfigPath"
+    }
+
+    $config = Get-Content $ConfigPath -Raw | ConvertFrom-Json
+    $scriptPath = $config.SetupRepoScriptPath
+
+    if (-not $scriptPath) {
+        throw "В конфиге не задан SetupRepoScriptPath"
+    }
+
+    if (-not (Test-Path $scriptPath)) {
+        throw "Файл не найден: $scriptPath"
+    }
+
+    & python $scriptPath
+
+    if ($LASTEXITCODE -ne 0) {
+        throw "Скрипт завершился с кодом $LASTEXITCODE"
+    }
+}
